63a572d1962926ae93b71d3c74b4419610fa808a
MizaelMuro01 <mizaelmuro@gmail.com>
2026-03-20 15:45:53 -0500

    147

--- week01/code/obj/Debug/net10.0/code.AssemblyInfo.cs
+++ week01/code/obj/Debug/net10.0/code.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("code")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8198850792c5ea5ffc8807cf3ecb46a6610fb4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd6d2f9dbaeb7222cd5844de5b4ecce47c9ef313")]
 [assembly: System.Reflection.AssemblyProductAttribute("code")]
 [assembly: System.Reflection.AssemblyTitleAttribute("code")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
