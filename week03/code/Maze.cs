40f7ab3d10535eaa8574881bc87a415c49a83bde
MizaelMuro01 <mizaelmuro@gmail.com>
2026-03-23 16:46:53 -0500

    159

--- week03/code/Maze.cs
+++ week03/code/Maze.cs
@@ -1,12 +1,55 @@
-public static (int x, int y) MoveLeft(int x, int y, Dictionary<(int x, int y), (bool left, bool right, bool up, bool down)> maze)
+using System;
+using System.Collections.Generic;
+
+class Maze
 {
-    // Check if current position exists in maze
-    if (!maze.ContainsKey((x, y)))
+    public static (int x, int y) MoveLeft(int x, int y, Dictionary<(int x, int y), (bool left, bool right, bool up, bool down)> maze)
+    {
+        if (!maze.ContainsKey((x, y)))
+            return (x, y);
+
+        if (maze[(x, y)].left)
+            return (x - 1, y);
+
         return (x, y);
-    
-    // Check if can move left
-    if (maze[(x, y)].left)
-        return (x - 1, y);
-    
-    return (x, y);
+    }
+
+    public static (int x, int y) MoveRight(int x, int y, Dictionary<(int x, int y), (bool left, bool right, bool up, bool down)> maze)
+    {
+        if (!maze.ContainsKey((x, y)))
+            return (x, y);
+
+        if (maze[(x, y)].right)
+            return (x + 1, y);
+
+        return (x, y);
+    }
+
+    public static (int x, int y) MoveUp(int x, int y, Dictionary<(int x, int y), (bool left, bool right, bool up, bool down)> maze)
+    {
+        if (!maze.ContainsKey((x, y)))
+            return (x, y);
+
+        if (maze[(x, y)].up)
+            return (x, y + 1);
+
+        return (x, y);
+    }
+
+    public static (int x, int y) MoveDown(int x, int y, Dictionary<(int x, int y), (bool left, bool right, bool up, bool down)> maze)
+    {
+        if (!maze.ContainsKey((x, y)))
+            return (x, y);
+
+        if (maze[(x, y)].down)
+            return (x, y - 1);
+
+        return (x, y);
+    }
 }
+
+// MoveLeft: go to x-1 if left is true
+// MoveRight: go to x+1 if right is true
+// MoveUp: go to y+1 if up is true
+// MoveDown: go to y-1 if down is true
+// If position not in maze or move not allowed, stay in same place
