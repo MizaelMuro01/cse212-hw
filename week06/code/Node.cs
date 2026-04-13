6be0eecb47facb402bfa465e653cc20529cb0750
MizaelMuro01 <mizaelmuro@gmail.com>
2026-04-13 17:33:58 -0500

    157

---
+++ week06/code/Node.cs
@@ -0,0 +1,53 @@
+public class Node
+{
+    public int Data { get; set; }
+    public Node? Right { get; private set; }
+    public Node? Left { get; private set; }
+
+    public Node(int data)
+    {
+        this.Data = data;
+    }
+
+    public void Insert(int value)
+    {
+        // Problem 1
+        if (value == Data)
+            return;
+
+        if (value < Data)
+        {
+            // Insert - left
+            if (Left is null)
+                Left = new Node(value);
+            else
+                Left.Insert(value);
+        }
+        else
+        {
+            // Insert - right
+            if (Right is null)
+                Right = new Node(value);
+            else
+                Right.Insert(value);
+        }
+    }
+
+    public bool Contains(int value)
+    {
+        if (value == Data)
+            return true;
+        else if (value < Data)
+            return Left?.Contains(value) ?? false;
+        else
+            return Right?.Contains(value) ?? false;
+    }
+
+    public int GetHeight()
+    {
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return 1 + Math.Max(leftHeight, rightHeight);
+
+    }
+}
